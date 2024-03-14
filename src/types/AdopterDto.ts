378d61d2142c92501c17c44de940988d63029cec
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-03-14 11:18:22 -0300

    Change in the fields photo and address to not required

--- src/types/AdopterDto.ts
+++ src/types/AdopterDto.ts
@@ -2,8 +2,8 @@ type AdopterDto = {
   name: string;
   password: string;
   phone: string;
-  photo: string;
-  address: string;
+  photo?: string;
+  address?: string;
 };
 
 export default AdopterDto;
